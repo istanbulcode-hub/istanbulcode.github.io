76788c1a42371376bbdadc1c7f5b1e2672cb6017
istanbulcode <info@istanbulcode.com>
2025-11-01 10:22:21 +0300

    Commit config.js

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,26 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-  return {
-    base: `/${env.VITE_REPO_NAME}/`,
-    plugins: [react(), tailwindcss()],
-    server: {
-      open: true,
-      host: false, // Allow access from network devices
-      proxy: {
-        "/api": {
-          target: "http://localhost:5000",
-          changeOrigin: true,
-          secure: true,
-        },
-      },
-    },
-  };
+// export default defineConfig(({ mode }) => {
+//   const env = loadEnv(mode, process.cwd(), "");
+//   return {
+//     base: `/${env.VITE_REPO_NAME}/`,
+//     plugins: [react(), tailwindcss()],
+//     server: {
+//       open: true,
+//       host: false, // Allow access from network devices
+//       proxy: {
+//         "/api": {
+//           target: "http://localhost:5000",
+//           changeOrigin: true,
+//           secure: true,
+//         },
+//       },
+//     },
+//   };
+// });
+
+export default defineConfig({
+  base: "./",
+  plugins: [react(), tailwindcss()],
 });
