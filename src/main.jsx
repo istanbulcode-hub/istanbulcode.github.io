1d25f110b0f78bc8d11efad057d447b133aeda4c
istanbulcode <info@istanbulcode.com>
2025-11-01 10:56:26 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,6 @@ import "../src/../index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider/>
   </React.StrictMode>
 );
